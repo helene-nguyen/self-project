5729bf07e28316db16ab2a6fc8e5e87c84144bde
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2022-03-04 11:38:51 +0100

    display page finished + adding some js hide and show

--- js/display.js
+++ js/display.js
@@ -1,1 +1,17 @@
 "use strict"
+
+//Display : none => onclick make disappear the blocks
+let btnNone = document.getElementById("btn-none");
+let btnShow = document.getElementById("btn-show");
+
+function hideBox() {
+    document.getElementsByClassName('btn-clicked')[0].style.display = "none";
+}
+
+//Here is the function for showing the block again
+function showBox() {
+    document.getElementsByClassName('btn-clicked')[0].style.display = "block";
+}
+
+btnNone.onclick = hideBox;
+btnShow.onclick = showBox;
